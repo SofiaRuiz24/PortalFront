e0c786d7b35eca4773007918336d0e42af7ce846
Martin Barrera <mbarrera203@gmail.com>
2025-02-04 13:39:57 -0300

    Se agregaron los botones de eliminar productos, manejo correcto de botones, y renderizado correcto con los nuevos campos de unidades

--- components/productos/productAll.tsx
+++ components/productos/productAll.tsx
@@ -41,12 +41,17 @@ import axios from "axios";
 import  Documentos  from "../../app/types/documentosType";
 
 export function ProductAll() {
+    //Estado para el manejo de categorias
     const { toast } = useToast();
-    //const [selectedCategory, setSelectedCategory] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
     const [products, setProducts] = useState<any[]>([]);
     const formRef = useRef<HTMLFormElement>(null); // REF para limpiar el formulario
     const [ documentosPreview, setDocumentosPreview ] = useState<Documentos[]>([]); // Vista previa de los documentos
+    const [isSubmittingUnits, setIsSubmittingUnits] = useState(false);
+    const [isSubmittingDelete, setIsSubmittingDelete] = useState(false);
+    const [tipoDoc, setTipoDoc] = useState("");
 
     //Función para obtener los productos de la API
     const fetchProducts = async () => {
@@ -64,10 +69,11 @@ export function ProductAll() {
         fetchProducts();
     }, [isSuccess]);
    
+    //Guarda los productos en la base de datos
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setIsSuccess(false);
-    
+        setIsSubmitting(true);//Desactiva el boton de guardar
         const formData = new FormData(e.currentTarget);
     
         try {
@@ -121,15 +127,35 @@ export function ProductAll() {
                 variant: "destructive",
                 duration: 5000,
             });
+        }finally{
+            setIsSubmitting(false); //Activa el boton de guardar
         }
     };
     
-    
-
+    //Guarda las unidades en la base de datos
     const handleSubmitUnidades = async (e: React.FormEvent<HTMLFormElement>, producto:string) => {
         e.preventDefault();
+        setIsSubmittingUnits(true); // Deshabilitar botón
+        
         const formData = new FormData(e.currentTarget);
         formData.append("producto", producto);
+
+          // Validar que todos los campos estén completos
+          const nSerie = formData.get("nSerie");
+          const antiguedad = formData.get("antiguedad");
+          const unitDocs = formData.getAll("unit-docs");
+  
+          if (!nSerie || !antiguedad || unitDocs.length === 0) {
+              toast({
+                  title: "Error",
+                  description: "Por favor, complete todos los campos antes de confirmar.",
+                  variant: "destructive",
+                  duration: 5000,
+              });
+              setIsSubmittingUnits(false); // Habilitar botón
+              return;
+          }
+          
         try {
             const response = await axios.post("http://localhost:4108/unidades", formData, {
                 headers: {
@@ -144,9 +170,11 @@ export function ProductAll() {
                     variant: "default",
                     duration: 5000,
                 });
-
-                // Limpiar formulario
-                e.currentTarget.reset();
+                
+                // Limpiar el formulario antes de cerrar el diálogo
+                const form = e.currentTarget;
+                //form.reset();
+                
             } else {
                 throw new Error("Respuesta inesperada del servidor");
             }
@@ -158,8 +186,12 @@ export function ProductAll() {
                 variant: "destructive",
                 duration: 5000,
             });
+        } finally {
+            setIsSubmittingUnits(false); // Habilitar botón
         }
     };
+
+    //Función para cargar los documentos
     const handleDocs = (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if (files) {
@@ -177,7 +209,39 @@ export function ProductAll() {
         }
     };
     
-    
+    //Función para eliminar un producto
+    const handleDelete = async (productId: string) => {
+        setIsSubmittingDelete(true);
+        try {
+            // Eliminar el producto y sus unidades
+            const response = await axios.delete(`http://localhost:4108/productos/${productId}`);
+            
+
+            if (response.status >= 200 && response.status < 300) {
+                toast({
+                    title: "Éxito",
+                    description: "Producto y sus unidades eliminados correctamente",
+                    variant: "default",
+                    duration: 5000,
+                });
+
+                // Actualizar la lista de productos
+                fetchProducts();
+            } else {
+                throw new Error("Error al eliminar el producto y sus unidades");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            toast({
+                title: "Error",
+                description: "Error al eliminar el producto y sus unidades",
+                variant: "destructive",
+                duration: 5000,
+            });
+        } finally {
+            setIsSubmittingDelete(false);
+        }
+    };
 
     return (
         <div className="flex flex-col gap-8 p-6">
@@ -299,75 +363,125 @@ export function ProductAll() {
                                 <TableHead>Categoría</TableHead>
                                 <TableHead>Subcategoria</TableHead>
                                 <TableHead>Acciones</TableHead>
+                                <TableHead>Eliminar</TableHead>
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            {/* Ejemplo de fila - Esto se reemplazará con datos reales */}
-                                {products?.map((product: any) => (
+                            {products?.map((product: any) => (
                                 <TableRow key={product._id}>
-                                 <TableCell>{product.nombre}</TableCell>
-                                 <TableCell>
-                                    {
-                                    //Poner la primer letra a mayuscula
-                                    product.categoria.charAt(0).toUpperCase() + product.categoria.slice(1)
-                                    }
-                                </TableCell>
-                                 <TableCell>
-                                    {
-                                     product.subcategoria.charAt(0).toUpperCase() + product.subcategoria.slice(1)
-                                    }
-                                 </TableCell>
-                                 <TableCell>
-                                 <Dialog>
-                                    <DialogTrigger  className="bg-accent/80 text-white px-4 py-2 rounded-lg hover:bg-primary/90 transition-all">Agregar Unidades</DialogTrigger>
-                                    <DialogContent className="sm:max-w-[425px] bg-white p-8">
-                                        <DialogHeader>
-                                        <DialogTitle>Editar unidades</DialogTitle>
-                                        <DialogDescription>
-                                            Agregue las unidades del producto.Haga click en confirmar al terminar.
-                                        </DialogDescription>
-                                        </DialogHeader>
-                                        <form action="http://localhost:4108/unidades" method="post" className="space-y-6" encType="multipart/form-data" onSubmit={(e) => handleSubmitUnidades(e, product._id)}>
-                                        <div>
-                                            <Label htmlFor="nSerie">Numero de Serie</Label>
-                                            <Input
-                                                id="nSerie"
-                                                name="nSerie"
-                                                placeholder="Ingrese el numero de serie"
-                                                required
-                                            />
-                                        </div>
-                                        <div>
-                                            <Label htmlFor="antiguedad">Año</Label>
-                                            <Input
-                                                id="antiguedad"
-                                                name="antiguedad"
-                                                placeholder="Ingrese el año del producto"
-                                                required
-                                            />
-                                        </div>
-                                        <div className="space-y-2">
-                                            <Label htmlFor="unit-docs">Cargar Documentación</Label>
-                                            <Input
-                                                id="unit-docs"
-                                                name="unit-docs"
-                                                type="file"
-                                                accept="application/pdf"
-                                                multiple
-                                                className="cursor-pointer"   
-                                            />
-                                        </div>
-                                            <DialogFooter>
-                                                <DialogClose type="submit" className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-primary/70 transition-all">
-                                                    Confirmar
-                                                </DialogClose>
-                                            </DialogFooter>   
-                                        </form>
-                                    </DialogContent>
-                                </Dialog>
-                                 </TableCell>    
-                                 </TableRow>
-                                ))}            
+                                    <TableCell>{product.nombre}</TableCell>
+                                    <TableCell>
+                                        {product.categoria.charAt(0).toUpperCase() + product.categoria.slice(1)}
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.subcategoria.charAt(0).toUpperCase() + product.subcategoria.slice(1)}
+                                    </TableCell>
+                                    <TableCell>
+                                        <Dialog>
+                                            <DialogTrigger className="bg-accent/80 text-white px-4 py-2 rounded-lg hover:bg-primary/90 transition-all">
+                                                Agregar Unidades
+                                            </DialogTrigger>
+                                            <DialogContent className="sm:max-w-[425px] bg-white p-8">
+                                                <DialogHeader>
+                                                    <DialogTitle>Editar unidades</DialogTitle>
+                                                    <DialogDescription>
+                                                        Agregue las unidades del producto.Haga click en confirmar al terminar.
+                                                    </DialogDescription>
+                                                </DialogHeader>
+                                                <form action="http://localhost:4108/unidades" method="post" className="space-y-6" encType="multipart/form-data" onSubmit={(e) => handleSubmitUnidades(e, product._id)}>
+                                                    <div>
+                                                        <Label htmlFor="nSerie">Numero de Serie</Label>
+                                                        <Input
+                                                            id="nSerie"
+                                                            name="nSerie"
+                                                            placeholder="Ingrese el numero de serie"
+                                                            required
+                                                        />
+                                                    </div>
+                                                    <div>
+                                                        <Label htmlFor="antiguedad">Año</Label>
+                                                        <Input
+                                                            id="antiguedad"
+                                                            name="antiguedad"
+                                                            placeholder="Ingrese el año del producto"
+                                                            required
+                                                        />
+                                                    </div>
+                                                    <div>
+                                                        <Label htmlFor="tipoDoc">Tipo de Documento</Label>
+                                                        <select name="selectDoc" id="selectDoc" onChange={(e) => setTipoDoc(e.target.value)} className="w-full p-2 border rounded-md">
+                                                            <option value="">Seleccione un tipo de documento</option>
+                                                            <option value="patente">Patente</option>
+                                                            <option value="certificadoA">Certificado A</option>
+                                                            <option value="certificadoB">Certificado B</option>
+                                                            <option value="certificadoC">Certificado C</option>
+                                                        </select>
+                                                    </div>
+                                                    { tipoDoc.includes("patente") ?
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="patente">Patente</Label>
+                                                        <Input
+                                                            id="patente"
+                                                            name="patente"
+                                                            type="file"
+                                                            accept="application/pdf"
+                                                            className="cursor-pointer"   
+                                                        />
+                                                    </div>:( tipoDoc.includes("certificadoA")?
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="certificadoA">Certificado A</Label>
+                                                        <Input
+                                                            id="certificadoA"
+                                                            name="certificadoA"
+                                                            type="file"
+                                                            accept="application/pdf"
+                                                            className="cursor-pointer"   
+                                                        />
+                                                    </div>:( tipoDoc.includes("certificadoB")?
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="certificadoB">Certificado B</Label>
+                                                        <Input
+                                                            id="certificadoB"
+                                                            name="certificadoB"
+                                                            type="file"
+                                                            accept="application/pdf"
+                                                            className="cursor-pointer"   
+                                                        />
+                                                    </div>:( tipoDoc.includes("certificadoC")?
+                                                    <div className="space-y-2">
+                                                        <Label htmlFor="unit-docs">Certificado C</Label>
+                                                        <Input
+                                                            id="certificadoC"
+                                                            name="certificadoC"
+                                                            type="file"
+                                                            accept="application/pdf"
+                                                            multiple
+                                                            className="cursor-pointer"   
+                                                        />
+                                                    </div>:  null
+                                                    )))}
+                                                    <DialogFooter>
+                                                        <button type="submit" className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-primary/70 transition-all" disabled={isSubmittingUnits}>
+                                                            Confirmar
+                                                        </button>
+                                                    </DialogFooter>   
+                                                </form>
+                                            </DialogContent>
+                                        </Dialog>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Button
+                                            variant="destructive"
+                                            size="sm"
+                                            onClick={() => handleDelete(product._id)}
+                                            className="bg-red-600 hover:bg-red-700"
+                                        >
+                                            {isSubmittingDelete ? "Eliminando..." : "Eliminar"}
+                                            <Trash2 className="h-4 w-4" />
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}            
                         </TableBody>
                     </Table>
                 </CardContent>
